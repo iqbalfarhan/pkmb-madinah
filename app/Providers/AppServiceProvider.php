14a04f6d95ba4703c228c6f10671181394bb060c
iqbalfarhan <iqbalfarhan1996@gmail.com>
2025-12-30 22:43:17 +0800

    fix json response

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+        
         // required
         Validator::replacer('required', function ($message, $attribute, $rule, $parameters) {
             return $attribute.' wajib diisi.';
